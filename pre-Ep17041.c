d3bb9722cf0b7256659e5808901a2971f6b7bad4
hogehoge <ep16018@sti.chubu.ac.jp>
2019-11-29 17:19:39 +0900

    gccss

--- pre-Ep17041.c
+++ pre-Ep17041.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main(void){
 	long x,n,m;
-	scanf("%ld",x);
-	scanf("%ld",n);
-	scanf("%ld",m);
+	scanf("%ld",&x);
+	scanf("%ld",&n);
+	scanf("%ld",&m);
 	for(int i=0;i<n;i++)
 		x*=x;
 	printf("%ld",x/m);
