b879a5ed9e15ed2744a75b9ac46b645ff1e4e697
hogehoge <ep16018@sti.chubu.ac.jp>
2019-11-29 18:08:07 +0900

    gccss

--- pre-ep17070.c
+++ pre-ep17070.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
 int main(void){
 	long i,a,x,n,m;
 	scanf("%ld %ld %ld",&x,&n,&m);
 	a=x;
 	for(i=1;i<n;i++){
 		x=x*a;
-		if(x>m){
-		x=x%m;	
-		}
+		if(x>m) x=x%m;		
 	}
 	printf("%ld\n",x);
 }
