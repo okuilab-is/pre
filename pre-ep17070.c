000c2817361940fcffec889cc914eefb7e2bb5c7
hogehoge <ep16018@sti.chubu.ac.jp>
2019-11-29 17:25:55 +0900

    gccss

---
+++ pre-ep17070.c
@@ -0,0 +1,17 @@
+#includ <stdio.h>
+
+int main void(){
+	int i;
+	long a,x,n,m;
+	scanf("%d",x);
+	scanf("%d",n);
+	scanf("%d",n);
+
+	a=x;
+
+	for(i=0;i<n;i++){
+		x*=a;
+	}
+	
+	printf("%d",x/m);
+}
