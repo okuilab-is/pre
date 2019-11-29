50701262b4c254b3535b3330f8cae8e1f1d51195
hogehoge <ep16018@sti.chubu.ac.jp>
2019-11-29 17:28:08 +0900

    gccss

--- per-ep17093.c
+++ per-ep17093.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 void main(){
- long x,n,m;
- scanf("%ld",&x);
- scanf("%ld",&n);
- scanf("%ld",&m);
-
- printf("%ld\n",x^n%m);
+  long x,n,m;
+  scanf("%ld",&x);
+  scanf("%ld",&n);
+  scanf("%ld",&m);
+  for (int i=0;i<n;i++){
+    x=x*x;
+  }
+  printf("%ld\n",x%m);
 }
