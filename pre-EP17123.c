f274d546f3f9a575ea0c1f476c4712b611600a1e
hogehoge <ep16018@sti.chubu.ac.jp>
2019-11-29 18:12:56 +0900

    gitss

--- pre-EP17123.c
+++ pre-EP17123.c
@@ -12,3 +12,16 @@ int main(void){
 	printf("%ld\n",a);
 	return 0;
 }
+
+//******
+
+int main(void){
+	int i;
+	long a=1,x,n,m;
+	scanf("%ld %ld %ld",&x,&n,&m);
+	for(i=0; i<m; i=i*i){
+		
+	}
+	printf("%ld\n",a);
+	return 0;
+}
