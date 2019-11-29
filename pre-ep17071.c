f864394e818ab8d1f69ce567d14b46a046ac9a11
hogehoge <ep16018@sti.chubu.ac.jp>
2019-11-29 17:49:39 +0900

    gccss

--- pre-ep17071.c
+++ pre-ep17071.c
@@ -7,7 +7,7 @@ int main(void){
   scanf("%ld %ld %ld",&x,&n,&m);//値入力
   for(int i=0;i<n;i++){//べき乗
     b*=x;
-    printf("%ld\n",b);
+    //printf("%ld\n",b);
   }
   x=b%m;//余りを求める
   printf("%ld\n",x);
