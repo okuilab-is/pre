330e7490c337836e98683deb2e8015dd979a4395
hogehoge <ep16018@sti.chubu.ac.jp>
2019-11-29 17:22:49 +0900

    gccss

--- pre-ep17071.c
+++ pre-ep17071.c
@@ -4,9 +4,9 @@ int main(void){
 
   long x,n,m;//数値x、べき乗数n、余り数m
 
-  scanf("%ld %ld %ld",x);//値入力
-  scanf("%ld",n);
-  scanf("%ld",m);
+  scanf("%ld %ld %ld",&x);//値入力
+  scanf("%ld",&n);
+  scanf("%ld",&m);
   for(int i=0;i<n;i++){//べき乗
     x*=x;
   }
