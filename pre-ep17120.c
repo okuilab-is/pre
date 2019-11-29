9d6537cb3f42aa3028f200511a73fe6ce2d9fd2e
hogehoge <ep16018@sti.chubu.ac.jp>
2019-11-29 17:21:02 +0900

    gccss

--- pre-ep17120.c
+++ pre-ep17120.c
@@ -2,12 +2,12 @@
 int main(void){
 	long x,n,m;
 	scanf("%ld%ld%ld",&x,&n,&m);
-	printf("%ld",x);
+	printf("%ld%ld%ld",x,n,m);
 
 	for(int i = 0;i<n;i++){
 		x*=x;
 	}
 	x%=m;
-	printf("%ld",x);
+	//printf("%ld",x);
 }
 	
