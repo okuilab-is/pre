2fd315d4de24023ac481f1f099bc99c1e587ec72
hogehoge <ep16018@sti.chubu.ac.jp>
2019-11-29 17:34:37 +0900

    gccss

--- pre-ep17071.c
+++ pre-ep17071.c
@@ -2,13 +2,13 @@
 #include <math.h>
 int main(void){
 
-  long x,n,m;//数値x、べき乗数n、余り数m
+  long x,n,m,ama;//数値x、べき乗数n、余り数m
 
   scanf("%ld %ld %ld",&x,&n,&m);//値入力
   for(int i=0;i<n;i++){//べき乗
     x=x*x;
-    x=x%m;
-    printf("%ld\n",x);
+    ama=x%m;
+    printf("%ld\n",ama);
   }
   //x=x%m;//余りを求める
 
