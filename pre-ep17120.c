eb6a9d70b05a9c81f9a4762ff63e7cbfd94c995f
hogehoge <ep16018@sti.chubu.ac.jp>
2019-11-29 17:43:26 +0900

    gccss

--- pre-ep17120.c
+++ pre-ep17120.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 int main(void){
-	long x,n,m;
+	long a,x,n,m;
+	a=1;
 	scanf("%ld%ld%ld",&x,&n,&m);
-	for(int i=1;i<n;i++){
-		x*=x;
+	for(int i=0;i<n;i++){
+		a*=x;
 	}
 	printf("%ld",x);
 	return 0;
